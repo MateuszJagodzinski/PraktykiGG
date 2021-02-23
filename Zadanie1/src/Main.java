e397429d16995ebb824387c080e23c29a9914bb5
MateuszJagodzinski <matiszkola@onet.pl>
2021-02-23 14:43:28 +0100

    Zadanie1

---
+++ Zadanie1/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Zadanie 1
+        int A = 10;
+        double B = 6.5;
+        char C = 'J';
+        String D = "Mati";
+        System.out.println(A+ " "+" " + B + " " + " " + C + " " + " " +D);
+
+        System.out.println("");
+
+        //Zadanie 2
+        int a = 10;
+        int b = 10;
+        int c = a*a+b*b;
+        System.out.println("Wynik:" +c);
+
+        System.out.println("");
+
+        //Zadanie 3
+        int T = 7*7;
+        double sqrt = Math.sqrt(3);
+        double myDoubleA = 49 * sqrt;
+        System.out.println(T*sqrt/4);
+
+        System.out.println("");
+
+        //Zadanie 4
+        String M = "Mateusz";
+        String J = "Jagodziński";
+        System.out.println(M+ " " +J);
+
+
+    }
+}
